86564850d5eed0f9139125e9f32a04d1b85726b3
kar18 <kazi-ar-rafi@protonmail.com>
2023-02-18 10:06:46 +0600

    update

---
+++ docs/feature_image_location_fixer.sh
@@ -0,0 +1,9 @@
+cp index.html old.index.html &&
+sed 's/.pngg/.png/' index.html > index.html &&
+sed 's/.pngng/.png/' index.html > index.html &&
+sed 's/.pngpng/.png/' index.html > index.html &&
+sed 's/.png.png/.png/' index.html > index.html &&
+sed 's/.png.pngg/.png/' index.html > index.html &&
+sed 's/.png.pngng/.png/' index.html > index.html &&
+sed 's/.png.pngpng/.png/' index.html > index.html &&
+sed 's/.png.png.png/.png/' index.html > index.html 
